2939a636f8b77e0123fe210275137809ae07691f
pravedens <pravedens@gmail.com>
2022-06-29 19:45:39 +0500

    setup vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import Vue from 'vue'
+import PostComponent from "./components/PostComponent";
+
+require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+
+    components: {
+        PostComponent,
+    }
+})
+
---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
