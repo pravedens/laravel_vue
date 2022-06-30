fcfcdac00849d557a4d190f7ac1ebf847449cb70
pravedens <pravedens@gmail.com>
2022-06-30 19:37:15 +0500

    methods

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,12 +1,36 @@
 <template>
     <div>
-
+        <SinglePostComponent></SinglePostComponent>
+        <div>Name: {{ name }}</div>
+        <button @click="sayHello">Hello</button>
+        <button @click="sayHi">Hi</button>
     </div>
 </template>
 
 <script>
+import SinglePostComponent from "./SinglePostComponent";
 export default {
-    name: "PostComponent"
+    name: "PostComponent",
+
+    data() {
+        return {
+            name: 'Vasya',
+            age: 20,
+        }
+    },
+
+    methods: {
+        sayHello() {
+            console.log('Hello')
+        },
+        sayHi() {
+            console.log('Hi')
+        }
+    },
+
+    components: {
+        SinglePostComponent
+    }
 }
 </script>
 
